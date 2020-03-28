9cfc5985d95e4938cf72ea367d58868b13251f4c
Tom Poulter <tom.poulter.1@outlook.com>
2020-03-28 12:30:07 +0000

    front page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'contact', to: "pages#contact", as: :contact
+  get 'about', to: "pages#about", as: :about
+  get 'services', to: "pages#services", as: :services
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
