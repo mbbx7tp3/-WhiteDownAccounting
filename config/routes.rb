d0d46239ea9a3f89e1783b5fb6f5223a022b54c3
Tom Poulter <tom.poulter.1@outlook.com>
2020-04-10 21:21:34 +0100

    footers added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'contact', to: "pages#contact", as: :contact
   get 'about', to: "pages#about", as: :about
   get 'services', to: "pages#services", as: :services
+  get 'disclaimer', to: "pages#disclaimer", as: :disclaimer
+  get 'equal', to: "pages#equal", as: :equal
+  get 'privacy', to: "pages#privacy", as: :privacy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
